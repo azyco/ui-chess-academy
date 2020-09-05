a134a4458f43edb26928aa4d4420b6d9dc02781d
Rahul Ghose <hansum.rahul@gmail.com>
2020-09-06 00:38:12 +0530

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
 import React from 'react';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 function App() {
   return (
-    <div className="ui-header">
-			"Hello World"
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Profile() {
+  return <h2>About</h2>;
+}
+
+function Login() {
+  return <h2>Login</h2>;
+}
+
 
 export default App;
