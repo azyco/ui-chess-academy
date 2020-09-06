128164a3f395e47fb3c9ba8d7e0e2eae38148af2
Rahul Ghose <hansum.rahul@gmail.com>
2020-09-06 13:33:57 +0530

    moved classes from functions

--- src/App.js
+++ src/App.js
@@ -8,51 +8,43 @@ import {
   Link
 } from "react-router-dom";
 
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { Profile } from './pages/Profile';
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function Profile() {
-  return <h2>About</h2>;
-}
-
-function Login() {
-  return <h2>Login</h2>;
-}
-
 
 export default App;
